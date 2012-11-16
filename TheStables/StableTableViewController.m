cd254d059d4eda007036aa2deb6c07e205ec663e
T. Andrew Binkowski <abinkowski@uchicago.edu>
2012-11-15 23:45:00 -0600

    Put reindeer in the data model instead of using raw data arrays

--- TheStables/StableTableViewController.m
+++ TheStables/StableTableViewController.m
@@ -7,13 +7,53 @@
 //
 
 #import "StableTableViewController.h"
+#import "Animal.h"
 
 @interface StableTableViewController ()
-@property (strong, nonatomic) NSArray *reindeer;
+@property (strong, nonatomic) NSMutableArray *reindeer;
 @end
 
 @implementation StableTableViewController
 
+/*******************************************************************************
+ * @method          fillTheStable
+ * @abstract        Create animal objects and put them in an array
+ * @description     
+ ******************************************************************************/
+- (void)fillTheStable
+{
+    // Raw data
+    NSArray *reindeerNames = [NSArray arrayWithObjects:@"Dasher",@"Dancer",@"Prancer",@"Vixen", @"Comet",@"Cupid",@"Donner",@"Blitzen",@"Rudolph",nil];
+    NSArray *reindeerURLs = @[
+        @"http://farm3.staticflickr.com/2151/2133661509_c54ce96fe5_q.jpg", @"http://farm3.staticflickr.com/2393/2133661737_3feae4ca89_q.jpg",
+        @"http://farm1.staticflickr.com/182/425617969_6cd61245b9_q.jpg", @"http://farm4.staticflickr.com/3042/2789917789_988c62c953_t.jpg",
+        @"http://farm5.staticflickr.com/4138/4882986746_e738e13515_t.jpg", @"http://farm6.staticflickr.com/5246/5218832836_5cce08e1d2_t.jpg",
+        @"http://farm3.staticflickr.com/2166/2158779446_5649063620_t.jpg", @"http://farm9.staticflickr.com/8044/8088504348_307589e545_t.jpg",
+        @"http://mostmetro.com/wp-content/uploads/2011/12/Rudolph-Red-Nosed-Reindeer-007.jpg"];
+    
+    // Initialize the reindeer property
+    _reindeer = [NSMutableArray array];
+    
+    // Create a new Animal object for each reindeer
+    Animal *currentAnimal;
+    for (int i=0; i<[reindeerNames count]; i++) {
+        currentAnimal = [[Animal alloc] init];
+        currentAnimal.type = @"Reindeer";
+        currentAnimal.name = [reindeerNames objectAtIndex:i];
+        currentAnimal.age = [NSNumber numberWithInt:100];
+        currentAnimal.imageURL = [NSURL URLWithString:[reindeerURLs objectAtIndex:i]];
+
+        // Add the object to the reindeer array
+        [self.reindeer addObject:currentAnimal];
+    }
+    NSLog(@">>>> Reindeer:%@",self.reindeer);
+}
+
+/*******************************************************************************
+ * @method          viewDidLoad
+ * @abstract        Called when view loads
+ * @description      
+ ******************************************************************************/
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -23,10 +63,7 @@
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    
-    _reindeer = [NSArray arrayWithObjects:@"Dasher",@"Dancer",@"Prancer",@"Vixen",
-                                        @"Comet",@"Cupid",@"Donner",@"Blitzen",@"Rudolph",nil];
-    NSLog(@">>>> Reindeer:%@",self.reindeer);
+    [self fillTheStable];
 }
 
 - (void)didReceiveMemoryWarning
@@ -55,7 +92,10 @@
                                                             forIndexPath:indexPath];
     
     // Configure the cell...
-    cell.textLabel.text = [self.reindeer objectAtIndex:indexPath.row];
+    // Get the current reindeer from the row index
+    Animal *currentAnimal = [self.reindeer objectAtIndex:indexPath.row];
+    cell.textLabel.text = currentAnimal.name;
+    cell.detailTextLabel.text = currentAnimal.type;    
     NSLog(@">>>> Asking for cell:%d for data:%@", indexPath.row,self.reindeer[indexPath.row]);
     return cell;
 }
@@ -103,6 +143,7 @@
    
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    NSLog(@">>>> Clicked on cell %d", indexPath.row);
     // Navigation logic may go here. Create and push another view controller.
     /*
      <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
@@ -111,5 +152,11 @@
      [self.navigationController pushViewController:detailViewController animated:YES];
      */
 }
+
+/*******************************************************************************
+ * @method          setImageForCell:fromUrl
+ * @abstract        Async donwload of image data from a passed URL; the image is assigned to the cell that is passed
+ * @description     WARNING: This is not a safe implementation of async downloading, check out AFNetworking on github for good example
+ ******************************************************************************/
 
 @end
