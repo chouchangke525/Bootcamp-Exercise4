6412211470903abb01b2dc97bb15bdbfd60acc7f
T. Andrew Binkowski <abinkowski@uchicago.edu>
2012-11-16 00:00:13 -0600

    Add Detail View Controller and hook up the interface

---
+++ TheStables/DetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.h
+//  TheStables
+//
+//  Created by T. Andrew Binkowski on 11/15/12.
+//  Copyright (c) 2012 The University of Chicago, Department of Computer Science. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *animalImage;
+@property (weak, nonatomic) IBOutlet UILabel *animalName;
+
+@end
